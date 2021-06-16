baa6f2d5784f113f9e652eee1184aff1851ce2ae
jbolozdina <jbolozdina@gmail.com>
2021-06-16 14:59:08 +0300

    Box/no box styles mockup

--- main.php
+++ main.php
@@ -2,11 +2,66 @@
 <html lang="en">
 <meta charset="UTF-8">
 <title>To-Do List</title>
+<!-- Init Bootstrap -->
+<link rel="stylesheet" href="style.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" 
 integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-<meta name="viewport" content="width=device-width,initial-scale=1">
+<!-- Init JS Bundle -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <body>
+
+<h2 id="top">To-Do List</h2>
+
+<div class="container">
+        <div class="task" id="t1">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="check1">
+                <label class="form-check-label" for="check1">
+                    <h4>Complete the assignment</h4>   
+                </label>
+            </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <button type="button" class="btn btn-warning">Edit</button>
+        </div>
+
+        <div class="row" id="t2">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="check2">
+                <label class="form-check-label" for="check2">
+                    <h4>Become smarter</h4>   
+                </label>
+            </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p id="creationTime">16.06 14:55</p>
+
+                <button type="button" class="btn btn-warning btn-sm float-end">Edit</button>
+
+        </div>
+
+        <div class="task" id="t3">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="check3">
+                <label class="form-check-label" for="check3">
+                    <h4>WeirdChamp</h4>   
+                </label>
+            </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                
+                
+                <button type="button" class="btn btn-warning">Edit</button>
+        </div>
+        <div class="row">
+            <button type="button" class="btn btn-success btn-sm">New</button>
+        </div>
+
+</div>
+
 </body>
 
 </html>

887017437041d087e2910d2c5eada9f8458ee396
jbolozdina <jbolozdina@gmail.com>
2021-06-16 16:44:23 +0300

    Testing design inconsistency

--- main.php
+++ main.php
@@ -24,8 +24,7 @@ integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0
                         <h4>Complete the assignment</h4>   
                     </label>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 <p id="creationTime">16.06 14:55</p>
                 <button type="button" class="btn btn-warning btn-sm float-end">Edit</button>
             </div>
@@ -39,7 +38,7 @@ integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0
                     </label>
                 </div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </p>
                     <p id="creationTime">16.06 14:55</p>
 
                     <button type="button" class="btn btn-warning btn-sm float-end">Edit</button>
